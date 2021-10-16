93efda737b57f70766cd41c2fe76bf1fa9cae527
marinaH <marinahortobagyi66@gmail.com>
2021-10-16 12:43:09 -0400

    One Test added

--- src/test/java/WebTest.java
+++ src/test/java/WebTest.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -31,5 +33,28 @@ public class WebTest {
 
     }
 
+    @Test
+    public void testSecond () throws InterruptedException {
+// 1 Arrange
+        String chromeDriver = "webdriver.chrome.driver";
+        String driverPath = "C://Users//marin//Documents/chromedriver.exe";
+        String url = "http://www.99-bottles-of-beer.net/";
+        String expectedResult = "Welcome to 99 Bottles of Beer";
+// 2 Act
+        System.setProperty(chromeDriver, driverPath);
+        WebDriver driver = new ChromeDriver();
+
+        driver.get(url);
+        Thread.sleep(3000);
+
+        WebElement header = driver.findElement(By.xpath("//div[@id = 'main']/h2"));
+        String actualResult = header.getText();
+// 3 Assert
+        Assert.assertEquals(actualResult, expectedResult);
+
+        driver.close();
+        driver.quit();
+
+    }
 }
 
